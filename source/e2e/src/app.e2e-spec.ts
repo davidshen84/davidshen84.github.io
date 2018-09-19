e7ea1b2b7d6bd263c279d58f44696fcb60d06fb8
Xi Shen <davidshen84@gmail.com>
2018-09-19 17:35:48 +0800

    Update packages and fix e2e tests.

--- source/e2e/src/app.e2e-spec.ts
+++ source/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toContain('Welcome to my blog');
+    expect(page.getParagraphText()).toContain('Welcome To My Blog');
   });
 });
