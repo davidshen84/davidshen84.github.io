3b175b1d24ce7b609ad27d36a7d54ce1ed1e07b5
Xi Shen <davidshen84@gmail.com>
2018-12-24 15:33:21 +0800

    Use HammerJS to handle pan action.

--- source/src/app/canvas-showcase/canvas-showcase.component.ts
+++ source/src/app/canvas-showcase/canvas-showcase.component.ts
@@ -1,6 +1,6 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {fromEvent} from 'rxjs';
-import {takeUntil, throttleTime} from 'rxjs/operators';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {fromEvent, Subscription} from 'rxjs';
+import {map, switchMap, takeUntil, throttleTime} from 'rxjs/operators';
 import {CanvasDrawService} from './canvas-draw.service';
 
 
@@ -9,15 +9,14 @@ import {CanvasDrawService} from './canvas-draw.service';
   templateUrl: './canvas-showcase.component.html',
   styleUrls: ['./canvas-showcase.component.scss']
 })
-export class CanvasShowcaseComponent implements OnInit {
-
-  @ViewChild('canvas')
-  private _canvasRef: ElementRef;
-
-  private _canvas: HTMLCanvasElement;
+export class CanvasShowcaseComponent implements OnInit, OnDestroy {
 
   public _points: Array<{}> = [];
+  @ViewChild('canvas')
+  private _canvasRef: ElementRef;
+  private _canvas: HTMLCanvasElement;
   private _canvasDraw: CanvasDrawService;
+  private _dragDropSubscription: Subscription;
 
   constructor(canvasDraw: CanvasDrawService) {
     this._canvasDraw = canvasDraw;
@@ -26,18 +25,28 @@ export class CanvasShowcaseComponent implements OnInit {
   ngOnInit() {
     this._canvas = this._canvasRef.nativeElement;
     this._canvasDraw.setCanvas(this._canvas);
-    this._canvasDraw.setPointOnCanvas(this._canvas.clientWidth / 2, this._canvas.clientHeight / 2, 255, 0, 0, 255);
-  }
 
-  public onMouseDown() {
-    fromEvent<MouseEvent>(this._canvas, 'mousemove').pipe(
-      throttleTime(500),
-      takeUntil(fromEvent(this._canvas, 'mouseup'))
-    ).subscribe(e => {
-      this._points.push({x: e.offsetX, y: e.offsetY});
-      // const {x, y} = this._canvasDraw.getCanvasCoordinates(e);
-      this._canvasDraw.setPointOnCanvas(e.offsetX, e.offsetY, 255, 0, 0, 255);
+    // Set a point at the center of the canvas.
+    this._canvasDraw.setPointOnCanvas(this._canvas.clientWidth / 2, this._canvas.clientHeight / 2, 255, 0, 0, 255);
+
+    // Define the drag-drop observable.
+    const target = new Hammer(this._canvas);
+    target.get('pan').set({direction: Hammer.DIRECTION_ALL});
+
+    const _dragDrop$ = fromEvent(target, 'panstart').pipe(
+      switchMap(() => fromEvent(target, 'panmove').pipe(
+        throttleTime(500),
+        takeUntil(fromEvent(target, 'panend')))),
+      map((e: HammerInput) => e.changedPointers[0] as PointerEvent)
+    );
+
+    this._dragDropSubscription = _dragDrop$.subscribe(({offsetX: x, offsetY: y}: PointerEvent) => {
+      this._points.push({x: x, y: y});
+      this._canvasDraw.setPointOnCanvas(x, y, 255, 0, 0, 255);
     });
   }
 
+  ngOnDestroy(): void {
+    this._dragDropSubscription.unsubscribe();
+  }
 }
