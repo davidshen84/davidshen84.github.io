46eb3cc95cc139507067ef24299f1698c1baf08e
Xi Shen <davidshen84@gmail.com>
2018-12-24 20:29:31 +0800

    Fix unit tests.

--- source/src/app/settings/settings.component.spec.ts
+++ source/src/app/settings/settings.component.spec.ts
@@ -1,6 +1,9 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SettingsComponent} from './settings.component';
+import {FormsModule} from '@angular/forms';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatExpansionModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -8,6 +11,13 @@ describe('SettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule
+      ],
       declarations: [SettingsComponent]
     })
       .compileComponents();
