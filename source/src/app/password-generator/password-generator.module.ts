68092ce415c98f1575b13510437c49ea0d2404a4
David Shen <davidshen84@gmail.com>
2020-07-18 14:53:14 +1000

    Remove ngx-clipboard
    Use cdk/clipboard instead.

--- source/src/app/password-generator/password-generator.module.ts
+++ source/src/app/password-generator/password-generator.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ClipboardModule } from 'ngx-clipboard';
 import { MaterialModules } from '../material.modules';
 import { PasswordGeneratorRoutingModule } from './password-generator-routing.module';
 import { PasswordGeneratorComponent } from './password-generator.component';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
   declarations: [PasswordGeneratorComponent],
