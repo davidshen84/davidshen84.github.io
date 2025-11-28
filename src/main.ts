19a971369dfff7fd57cd49b64904567416f06485
David Shen <davidshen84@gmail.com>
2025-11-29 10:07:32 +1100

    feat: upgrade to ng 21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import {
+  enableProdMode,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
@@ -40,6 +44,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection(),
     provideRouter(routes, withHashLocation()),
     importProvidersFrom(
       BrowserModule,
